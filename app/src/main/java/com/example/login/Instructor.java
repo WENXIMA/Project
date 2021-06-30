e2a128a8cdd0a260014aeb9b64f4d9e19810eaaa
WENXIMA <55809402+WENXIMA@users.noreply.github.com>
2021-06-30 14:00:59 -0400

    Delete app directory

--- app/src/main/java/com/example/login/Instructor.java
+++
@@ -1,9 +0,0 @@
-package com.example.login;
-
-public class Instructor extends User{
-    public Instructor(){}
-
-    public Instructor(String name, String password){
-        super(name, password);
-    }
-}
--- app/src/main/java/com/example/login/Student.java
+++
@@ -1,9 +0,0 @@
-package com.example.login;
-
-public class Student extends User{
-    public Student(){}
-
-    public Student(String name, String password){
-        super(name, password);
-    }
-}
