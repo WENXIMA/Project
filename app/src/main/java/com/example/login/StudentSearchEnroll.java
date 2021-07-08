50939bdd3ed4288741917b9953134d16d150422f
Lara <laralim10@gmail.com>
2021-07-08 17:53:27 -0400

    started student search/enroll page

---
+++ app/src/main/java/com/example/login/StudentSearchEnroll.java
@@ -0,0 +1,26 @@
+package com.example.login;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StudentSearchEnroll extends AppCompatActivity {
+//    TextView
+//    EditText
+//    Button
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_search_enroll);
+
+        // set IDs
+    }
+
+    // add, enroll, unenroll methods
+}
